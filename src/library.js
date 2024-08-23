00abb98199659fe451709c57bca19f8cf4abc5d4
Oscar Cañedo <oscar.r.canedo@gmail.com>
2024-08-23 21:45:09 +0200

    04 - REFACTOR functionality to pass test for adding multiple new books

--- src/library.js
+++ src/library.js
@@ -1,7 +1,14 @@
 const library = [];
 
 function addBookToLibrary(book) {
-    library.push(book);
+    if (Array.isArray(book)) {
+        // IF book is an array, ADD each book individually
+        book.forEach(b => library.push(b));
+    } else {
+        // IF book is a single object, ADD it directly
+        library.push(book);
+    }
+
 }
 
 module.exports = { addBookToLibrary, library };
