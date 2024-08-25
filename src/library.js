f431fe62176cb4b4e2f02945d9dc882b98d0b902
Oscar Cañedo <oscar.r.canedo@gmail.com>
2024-08-25 16:42:11 +0200

    05 - REFACTOR functionality to pass test for empty title field

--- src/library.js
+++ src/library.js
@@ -2,13 +2,20 @@ const library = [];
 
 function addBookToLibrary(book) {
     if (Array.isArray(book)) {
-        // IF book is an array, ADD each book individually
-        book.forEach(b => library.push(b));
+        // If `book` is an array, iterate and add each book individually
+        book.forEach(b => {
+            if (!b.title) {
+                throw new Error('Each book must have a title');
+            }
+            library.push(b);
+        });
     } else {
-        // IF book is a single object, ADD it directly
+        // If `book` is a single object, check for a title before adding
+        if (!book.title) {
+            throw new Error('Book must have a title');
+        }
         library.push(book);
     }
-
 }
 
 module.exports = { addBookToLibrary, library };
